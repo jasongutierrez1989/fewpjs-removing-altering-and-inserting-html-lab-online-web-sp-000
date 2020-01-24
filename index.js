4fcb8fe04cecad8c888070baf105eda1726f3899
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-01-24 00:01:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove();
+
+let newHeader = document.querySelector("head");
+newHeader.innerHTML = "<h1 id="victory">YOUR-NAME is the champion</h1>"
+document.appendChild(newHeader)
